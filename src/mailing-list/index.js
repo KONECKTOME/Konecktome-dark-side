9ec143d9fc02f4000a64c29747106b67ef44778f
QOA <58850207+QAKANDE@users.noreply.github.com>
2022-02-09 12:26:20 +0000

    push

--- src/mailing-list/index.js
+++ src/mailing-list/index.js
@@ -114,7 +114,7 @@ router.post("/transactional", async (req, res) => {
     <h3>Congratulations ${fName}, welcome to the KONECKTOME family!</h3>
     <p>If you’re receiving this email, it means you’re now officially a proud member of the KONECKTOME family! </p>
     <p> We’re extremely excited you’re able to be part of our ‘Road To Launch’ journey where you’ll get to know and experience what being part of our family means and what we stand for.
-  Expect some exciting and engaging content coming your way that will make you feel more ‘koneckted’ than ever before.</p>
+           Expect some exciting and engaging content coming your way that will make you feel more ‘koneckted’ than ever before.</p>
       <p>Stop yourself from missing out on awesome content by making sure you’ve got our email address saved to your contact or address book. This stops our emails from being accidentally marked as spam when you receive them.</p>
       <p>Why not also invite your friends and family to be part of our awesome family journey.</p>
     <p>Catch you soon,</p>
